2edcd91d730c963176052147662a19ea5db53873
Sandun Rajitha <rajitha1911@gmail.com>
2020-12-10 20:57:15 +0530

    List view implemented

--- YCombinator News Reader/ContentView.swift
+++ YCombinator News Reader/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            
+            List(posts) { post in
+                Text(post.title)
+            }
+            .navigationBarTitle(
+                Text("YCombinator News")
+                    .font(.system(size: 200, weight: .black))
+            )
+        }
     }
 }
+
+struct Post: Identifiable {
+    var id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "bla bla 1"),
+    Post(id: "2", title: "bla bla 2"),
+    Post(id: "3", title: "bla bla 3"),
+    Post(id: "4", title: "bla bla 4"),
+    Post(id: "5", title: "bla bla 5")
+]
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12 Pro")
     }
 }
