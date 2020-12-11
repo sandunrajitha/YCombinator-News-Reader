901046e1d39e8b21ec57ca03b483386daa9f819a
Sandun Rajitha <rajitha1911@gmail.com>
2020-12-11 19:00:41 +0530

    optional handled while creating the url

--- YCombinator News Reader/Model/NetworkManager.swift
+++ YCombinator News Reader/Model/NetworkManager.swift
@@ -9,9 +9,8 @@ import Foundation
 
 class NetworkManager {
     func fetchData() {
-        let apiURL = URL(string: "https://hn.algolia.com/api/v1/search?tags=front_page")
         
-        if let url = apiURL{
+        if let url = URL(string: "https://hn.algolia.com/api/v1/search?tags=front_page"){
         
         let session = URLSession.shared.dataTask(with: url) { (data, response, error) in
             if let e = error{
